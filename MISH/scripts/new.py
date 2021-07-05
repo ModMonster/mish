490061009f16a7b3ccdea12f549a90203ac942dc
ModMonster <modmonster2060@gmail.com>
2021-07-05 15:19:50 -0400

    Add new command.

---
+++ MISH/scripts/new.py
@@ -0,0 +1,50 @@
+import os
+
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+file = open(root + "/shell/command.data", "r") # open file in read mode
+commands = file.read() # read commands
+args = commands.split(" ") # save args
+file = open(root + "/shell/dir.data", "r") # open file in read mode
+dir = file.read() # read dir
+file.close() # close file
+
+newDir = ""
+
+if (len(args) >= 2):
+    if (args[1] != ""):
+        # remove last "/"
+        if ((args[1][-1] == "/") & (args[1][0] != "/")):
+            args[1] = args[1][:-1]
+
+        # should be absolute?
+        if (args[1][0] == "/"):
+            newDir = args[1]
+        else:
+            # in root?
+            if (dir == "/"):
+                newDir = dir + args[1]
+            else:
+                newDir = dir + "/" + args[1]
+
+        # dot fixes
+        newDir = os.path.abspath(root + newDir) # get rid of ..
+        newDir = newDir.replace(root, "", 1) # remove root path
+        newDir = newDir.replace("\\", "/") # replace \ with /
+
+        # fix "nodir"
+        if (newDir == ""):
+            newDir = "/"
+        
+        if (os.path.isdir(os.path.dirname(root + newDir))):
+            if (not os.path.exists(root + newDir)):
+                newFile = open(root + newDir, "w")
+                newFile.close()
+            else:
+                print("File or directory already exists.")
+        else:
+            print("Parent directory of file is a file.")
+    else:
+        print("You need to specify a name for the new file.")
+else:
+    print("You need to specify a name for the new file.")
